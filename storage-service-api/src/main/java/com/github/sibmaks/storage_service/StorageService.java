cad3f7aea0d6acce35417f8ba0e59a8faf57be64
sibmaks <sibmaks@vk.com>
2023-04-12 17:48:32 +0300

    Add docs for service services

--- storage-service-api/src/main/java/com/github/sibmaks/storage_service/StorageService.java
+++ storage-service-api/src/main/java/com/github/sibmaks/storage_service/StorageService.java
@@ -9,11 +9,29 @@ import java.util.Map;
  * @since 2023-04-11
  */
 public interface StorageService {
-
+    /**
+     * Storage type code. Must be unique in spring context. <br/>
+     * Beware of changing it, if some data already presented in DataBase.
+     *
+     * @return storage type code
+     */
     String getStorageCode();
 
+    /**
+     * Get content from content store
+     *
+     * @param id content id
+     * @return content
+     */
     Content get(String id);
 
+    /**
+     * Create content in content store
+     *
+     * @param meta meta attributes of content
+     * @param content content as binary array
+     * @return content id
+     */
     String create(Map<String, String> meta, byte[] content);
 
 }
