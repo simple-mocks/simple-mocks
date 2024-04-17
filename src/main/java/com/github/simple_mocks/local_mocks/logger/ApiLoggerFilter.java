e97bd48b733f269bac6947305c2f89265fa18d55
sibmaks <sibmaks@vk.com>
2024-04-17 10:42:09 +0300

    Improve rq/rs logging

--- src/main/java/com/github/simple_mocks/local_mocks/logger/ApiLoggerFilter.java
+++ src/main/java/com/github/simple_mocks/local_mocks/logger/ApiLoggerFilter.java
@@ -20,10 +20,13 @@ import java.util.UUID;
  */
 @Slf4j
 public class ApiLoggerFilter extends HttpFilter {
-    private static final Set<String> FULL_PRINT_CONTENT_TYPE = Set.of(
-            MediaType.TEXT_PLAIN_VALUE,
-            MediaType.APPLICATION_JSON_VALUE,
-            MediaType.APPLICATION_XML_VALUE
+    private static final Set<MediaType> FULL_PRINT_CONTENT_TYPE = Set.of(
+            MediaType.TEXT_PLAIN,
+            MediaType.TEXT_HTML,
+            MediaType.TEXT_XML,
+            MediaType.TEXT_MARKDOWN,
+            MediaType.APPLICATION_JSON,
+            MediaType.APPLICATION_XML
     );
 
     @Override
@@ -42,40 +45,110 @@ public class ApiLoggerFilter extends HttpFilter {
         } finally {
             var timing = System.currentTimeMillis() - startTime;
             var status = responseWrapper.getStatus();
+            var rqHeaders = getHeaders(requestWrapper);
             var rqType = requestWrapper.getContentType();
             var rqBody = getRqBody(requestWrapper);
             var rsType = responseWrapper.getContentType();
             var rsBody = getRsBody(responseWrapper);
             responseWrapper.copyBodyToResponse();
             log.info("""
-                    [INCOMING]
-                    RqUID: '{}'
-                    Method: '{}'
-                    Uri: '{}'
-                    Timing: '{} ms'
-                    Status: '{}'
-                    Rq-Content-Type: '{}'
-                    Rq: '{}'
-                    Rs-Content-Type: '{}'
-                    Rs: '{}'""", rqUID, method, requestURI, timing, status, rqType, rqBody, rsType, rsBody);
+                            [INCOMING]
+                            RqUID: '{}'
+                            Method: '{}'
+                            Uri: '{}'
+                            Timing: '{} ms'
+                            Status: '{}'
+                            Rq-Headers: '{}'
+                            Rq-Content-Type: '{}'
+                            Rq: '{}'
+                            Rs-Content-Type: '{}'
+                            Rs: '{}'""",
+                    rqUID,
+                    method,
+                    requestURI,
+                    timing,
+                    status,
+                    rqHeaders,
+                    rqType,
+                    rqBody,
+                    rsType,
+                    rsBody
+            );
         }
+    }
+
+    private String getHeaders(ContentCachingRequestWrapper requestWrapper) {
+        var headers = new StringBuilder();
+        var headerNames = requestWrapper.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            var header = headerNames.nextElement();
+            headers.append(header)
+                    .append("=\"")
+                    .append(getHeaderValue(requestWrapper, header))
+                    .append('"');
+            if (headerNames.hasMoreElements()) {
+                headers.append('\n');
+            }
+        }
+        return headers.toString();
+    }
+
+    private String getHeaderValue(ContentCachingRequestWrapper request, String headerName) {
+        var headerValues = request.getHeaders(headerName);
+        var headerValue = new StringBuilder();
+        while (headerValues.hasMoreElements()) {
+            var value = headerValues.nextElement();
+            headerValue.append(value);
+            if (headerValues.hasMoreElements()) {
+                headerValue.append(',');
+            }
+        }
+        return headerValue.toString();
     }
 
     private String getRqBody(ContentCachingRequestWrapper request) {
         var contentType = request.getContentType();
-        if (contentType != null && FULL_PRINT_CONTENT_TYPE.contains(contentType)) {
-            byte[] contentAsByteArray = request.getContentAsByteArray();
-            return new String(contentAsByteArray, StandardCharsets.UTF_8);
+        if (contentType == null || isNotPrintableContentType(contentType)) {
+            return "%d bytes".formatted(request.getContentLength());
         }
-        return "%d bytes".formatted(request.getContentLength());
+        try {
+            var inputStream = request.getInputStream();
+            var content = inputStream.readAllBytes();
+            if (content.length == 0) {
+                content = request.getContentAsByteArray();
+            }
+            return new String(content, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            log.error("Can't read", e);
+            return "can't read: " + e.getMessage();
+        }
     }
 
     private String getRsBody(ContentCachingResponseWrapper response) {
         var contentType = response.getContentType();
-        if (contentType != null && FULL_PRINT_CONTENT_TYPE.contains(contentType)) {
-            byte[] contentAsByteArray = response.getContentAsByteArray();
-            return new String(contentAsByteArray, StandardCharsets.UTF_8);
+        if (contentType == null || isNotPrintableContentType(contentType)) {
+            return "%d bytes".formatted(response.getContentSize());
         }
-        return "%d bytes".formatted(response.getContentSize());
+        try {
+            var inputStream = response.getContentInputStream();
+            var content = inputStream.readAllBytes();
+            if (content.length == 0) {
+                content = response.getContentAsByteArray();
+            }
+            return new String(content, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            log.error("Can't read", e);
+            return "can't read: " + e.getMessage();
+        }
+    }
+
+    private boolean isNotPrintableContentType(String contentType) {
+        var mediaType = MediaType.parseMediaType(contentType);
+        for (var printableContentType : FULL_PRINT_CONTENT_TYPE) {
+            if (printableContentType.isCompatibleWith(mediaType)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
